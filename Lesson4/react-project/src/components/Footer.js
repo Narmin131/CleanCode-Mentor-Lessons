e22ae9666a7e817c2c745a10bd42b56fdd1b39b8
Narmin <mnermin064@gmail.com>
2023-04-30 12:34:53 +0400

    React-introduction

---
+++ Lesson4/react-project/src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Footer = () => {
+
+  return (
+    <div style={{width:'100%', height:'50vh', backgroundColor:'red'}}>Footer</div>
+  )
+}
+
+export default Footer
